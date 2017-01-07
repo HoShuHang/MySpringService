a91e53f51f573a55f61c87deb796d70bd2c0147a
HoShuHang <kwite2002@gmail.com>
2017-01-08 01:27:20 +0800

    first commit

---
+++ src/main/java/com/selab/MySpringServiceApplication.java
@@ -0,0 +1,12 @@
+package com.selab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringServiceApplication.class, args);
+	}
+}
